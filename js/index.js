df4152c8dcf53588c1badc0bac3e830a169e26ea
aarshgarg <aarshgarg0@gmail.com>
2022-04-25 09:52:07 +0530

    variable and custom

---
+++ js/index.js
@@ -0,0 +1,10 @@
+  // JavaScript API
+  console.log("Hello world");
+  console.log("Hello World",4+7,"Another log");
+  alert("Warning");
+  document.write("This is used for the write in the script");
+  console.warn("Warning Danger");
+  console.error("Big Error");
+  var1=20;
+  var2=60;
+  console.log(var1+var2);
