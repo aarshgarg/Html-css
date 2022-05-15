cd550a569c79c2b604ee08ed7a53dca7ef91b617
aarshgarg <aarshgarg0@gmail.com>
2022-05-15 11:22:27 +0530

    Variable let and const

---
+++ js/variable_Let_Const.js
@@ -0,0 +1,28 @@
+console.log("Welcome Back to variable and constant ");
+// variale in js 
+//Variables,let const
+var name="Aarsh";
+var channel;
+var marks= 6778;
+marks=0;
+channel="Aarsh web page";
+console.log (name,channel,marks);
+
+var city = 'Delhi';
+console.log(city);
+
+const ownersName = 'Hari Ram';
+ //ownersName = 'Aarsh'; // Cannot do this (error)
+console.log(ownersName);
+
+{
+    let city = 'Rampur';   
+    city = 'Kolkata';
+    console.log(city);
+   }
+
+   console.log(city);
+
+   const arr1=[34,66,7,78,32];
+   arr1.push(51);
+   console.log(arr1);
