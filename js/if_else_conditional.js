e9d49bbda5824361a33e236ad4633a8755eafa67
aarshgarg <aarshgarg0@gmail.com>
2022-06-13 09:05:32 +0530

    if else

---
+++ js/if_else_conditional.js
@@ -0,0 +1,50 @@
+console.log('This is tutorial 8');
+const marr=19;
+const age = 128;
+const doesDrive='false';
+const sss=65;
+if (marr !=19)
+{
+    console.log('Age is not 19');
+}
+else
+{
+    console.log('Age is 19');
+}
+
+if(doesDrive || marr>=19)
+{
+    console.log("People can drive the car");
+}
+else{
+    console.log("Not drive");
+}
+
+if(sss !==65)
+{
+    console.log("Age is not 65");
+
+}
+
+else{
+    console.log("Age is 65");
+}
+
+
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+
+    case 28:
+        console.log("You are 28");
+        break;
+
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+        break;
+}         
